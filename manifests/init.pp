f0afd12d8dcc3a9840d9c6bc70cab193eb4eb026
Cameron <cameron.howarth@smsmt.com>
2014-04-17 11:28:35 +1000

    first commit

---
+++ manifests/init.pp
@@ -0,0 +1,61 @@
+#Copyright 2013 Dylan Ratcliffe
+#
+#Licensed under the Apache License, Version 2.0 (the "License");
+#you may not use this file except in compliance with the License.
+#You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#Unless required by applicable law or agreed to in writing, software
+#distributed under the License is distributed on an "AS IS" BASIS,
+#WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#See the License for the specific language governing permissions and
+#limitations under the License.
+
+define winoptionalfeature($feature_name = $title, $ensure, $restart = false, $logpath = '') {
+    if $restart == true { $strrestart = '' } else { $strrestart = '-norestart' }
+    if $logpath != '' { $strlogfile = "-logPath \"${logpath}\"" } else { $strlogfile = "" }
+    $strfeature_name = "-FeatureName ${feature_name}"
+    
+    if $ensure == 'present'{
+        $cmd = "Enable-WindowsOptionalFeature -Online ${strfeature_name} ${strrestart} ${strlogfile} "
+        $input = $cmd
+        $admin_wrapped_cmd = "start-process powershell -ArgumentList '-noprofile -command ${input}' -verb RunAs -wait -WindowStyle Hidden"
+        exec { "winoptionalfeature-install-feature-${strfeature_name}" :
+            command   => $admin_wrapped_cmd,
+            # FIXED IT WOO!
+            onlyif    => [
+                            "start-process powershell -ArgumentList '-noprofile -command New-Item HKCU:\\Software\\puppet -Name State -Value ((Get-WindowsOptionalFeature -online -FeatureName ${feature_name}).State) -Force' -verb RunAs -Wait -WindowStyle Hidden",
+                            "if (((Get-ItemProperty HKCU:\\Software\\puppet\\state).'(default)') -eq \"Enabled\") { exit 1 }"
+                         ],
+            logoutput => true,
+            provider  => powershell,
+        }
+            
+        notify {"winoptionalfeature-add-msg-${strfeature_name}":
+            message => "Invoking Enable-WindowsOptionalFeature: ${admin_wrapped_cmd}",
+        }
+            
+        Notify["winoptionalfeature-add-msg-${strfeature_name}"] -> Exec["winoptionalfeature-install-feature-${strfeature_name}"]
+    }
+    elsif $ensure == 'absent'{
+        $cmd = "Disable-WindowsOptionalFeature -Online ${strfeature_name} ${strrestart} ${strlogfile} "
+        $input = $cmd
+        $admin_wrapped_cmd = "start-process powershell -ArgumentList '-noprofile -command ${input}' -verb RunAs -wait -WindowStyle Hidden"
+        exec { "winoptionalfeature-remove-feature-${strfeature_name}" :
+            command   => $admin_wrapped_cmd,
+            onlyif    => [
+                            "start-process powershell -ArgumentList '-noprofile -command New-Item HKCU:\\Software\\puppet -Name State -Value ((Get-WindowsOptionalFeature -online -FeatureName ${feature_name}).State) -Force' -verb RunAs -wait -WindowStyle Hidden",
+                            "if (((Get-ItemProperty HKCU:\\Software\\puppet\\state).'(default)') -eq \"Disabled\") { exit 1 }"
+                         ],
+            logoutput => true,
+            provider  => powershell,
+           }
+        
+        notify {"winoptionalfeature-remove-msg-${strfeature_name}":
+            message => "Invoking Disable-WindowsOptionalFeature: ${admin_wrapped_cmd}",
+        }
+            
+        Notify["winoptionalfeature-remove-msg-${strfeature_name}"] -> Exec["winoptionalfeature-remove-feature-${strfeature_name}"]
+    }
+}
